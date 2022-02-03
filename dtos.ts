89188a72fa808767ed0600a00cb142ac1cd5d03d
Joel Kurian <joeltkurian@gmail.com>
2022-02-03 15:01:51 -0600

    added dtos

---
+++ dtos.ts
@@ -0,0 +1,43 @@
+export interface Reservation {
+    id: string,
+    checkIn: number,
+    checkOut: number,
+    owner: string,
+    room: string
+}
+
+export interface Activity {
+    id: string,
+    title: string,
+    desc: string,
+    startTime: number,
+    endTime: number,
+    location: string,
+    status: "On Schedule" | "Cancelled"
+}
+
+export interface ServiceRequest {
+    id: string,
+    room: string,
+    created: number,
+    status: "Ordered" | "Processing" | "Completed" | "Cancelled",
+    requestedOffering: Offering[]
+}
+
+export interface Offering {
+    desc: string,
+    cost: number
+}
+
+export interface ProblemSubmission {
+    desc: string,
+    base64Photo?: string
+}
+
+export interface Problem {
+    id: string,
+    submittedTime: number,
+    desc: string,
+    status: "Unreviewed" | "Reviewed",
+    photoLink?: string
+}

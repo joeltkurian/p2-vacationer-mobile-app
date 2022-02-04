b73da1673c8ffa861ade42e67b7bd7b69b1f6cf0
Daniel J Belenski <38082687+Belesnkid@users.noreply.github.com>
2022-02-04 15:08:44 -0500

    Create Order Component Laid Out

--- room-service/create-order-component.tsx
+++ room-service/create-order-component.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, Image, Button, FlatList, ScrollView } from "react-native";
+import { Text, View, Image, Button, FlatList, ScrollView, Dimensions } from "react-native";
 import { Offering } from '../dtos';
 
 export default function CreateOrderComponent(){
@@ -7,14 +7,22 @@ export default function CreateOrderComponent(){
     const dummy:Offering[] = [
         {desc:"Chicken Parm*Lorem Ipsum About Chicken Parm", cost:21.50},
         {desc:"Shrimp Scampi*Lorem Ipsum About Shrimp Scampi", cost:24.75},
-        {desc:"Rice Balls*Lorem Ipsum About Rice Balls", cost:14}
+        {desc:"Rice Balls*Lorem Ipsum About Rice Balls Lorem Ipsum About Rice Balls Lorem Ipsum About Rice Balls Lorem Ipsum About Rice Balls Lorem Ipsum About Rice Balls Lorem Ipsum About Rice Balls Lorem Ipsum About Rice Balls Lorem Ipsum About Rice Balls Lorem Ipsum About Rice Balls Lorem Ipsum About Rice Balls Lorem Ipsum About Rice Balls Lorem Ipsum About Rice Balls", cost:14},
+        {desc:"Shrimp Scampi*Lorem Ipsudm About Shrimp Scampi Lorem Ipsudm About Shrimp Scampi Lorem Ipsudm About Shrimp Scampi Lorem Ipsudm About Shrimp Scampi Lorem Ipsudm About Shrimp Scampi", cost:24.75},
+        {desc:"Shrimp Scampi*Lorem Ipsum About Shrismp Scampi", cost:24.75},
+        {desc:"Shrimp Scampi*Lorems Ipsum About Shrimp Scampi", cost:24.75},
+        {desc:"Shrimdp Scampi*Loresm fIpsum About Shrimp Scampi", cost:24.75},
+
     ];
 
+    const scrWidth = Dimensions.get('window').width;
+    const scrHeight = Dimensions.get('window').height;
+
     return(
-        <ScrollView style={{height:'100%', width:'100%', paddingTop:30}}>
-            <Text>Create Order Component</Text>
+        <View style={{height:scrHeight, width:scrWidth, paddingTop:30, alignContent:"center", paddingLeft:2, paddingRight:2}}>
+            <Text style={{alignSelf:"center"}}>Create Order Component</Text>
             <FlatList data={dummy} renderItem={({item}) => offeringItem({off:item})} keyExtractor={item => item.desc}/>
-        </ScrollView>
+        </View>
     )
 }
 
@@ -23,16 +31,19 @@ function offeringItem(props:{off:Offering}){
     const [title, descr] = desc.split('*');
     const imageLink = 'https://jtkbackgroundimgs.blob.core.windows.net/jtkreimbursementbackgrounds/question-mark.png';
 
+    const scrWidth = Dimensions.get('window').width;
+    const scrHeight = Dimensions.get('window').height;
+
     return(
-        <View style={{flexDirection:"row", paddingTop:5}}>
-            <View style={{flexDirection:"column"}}>
+        <View style={{flexDirection:"row", width:"100%", height:135}}>
+            <View style={{flexDirection:"column", height:128, width:128,}}>
                 <Image style={{height:128, width:128}} source={{uri:imageLink}}/>
             </View>
-            <View style={{flexDirection:"column", alignItems: 'center', alignContent: 'center', justifyContent: 'center'}}>
+            <View style={{flexDirection:"column", alignItems: 'center', justifyContent: 'center', alignContent:"center", width:scrWidth-130, maxHeight:135}}>
                 <Text>{title}</Text>
-                <Text>{descr}</Text>
-                <View style={{flexDirection:"row", justifyContent:"space-evenly"}}>
-                    <Text>{cost}</Text>
+                <Text style={{maxHeight:70}}>{descr}</Text>
+                <View style={{flexDirection:"row", justifyContent:"space-between", alignItems:"center", width:scrWidth-130-20}}>
+                    <Text>{"$" + cost.toFixed(2)}</Text>
                     <Button onPress={()=>{}} title="Add to Order"></Button>
                 </View>
             </View>
--- room-service/room-service-container.tsx
+++ room-service/room-service-container.tsx
@@ -1,16 +1,25 @@
-import { StyleSheet, View } from "react-native";
+import React from "react";
+import { StyleSheet, View, Dimensions, Button } from "react-native";
 import CartComponent from "./cart-component";
 import CompletedOrders from "./completed-orders";
 import CreateOrderComponent from "./create-order-component";
 
 export default function RoomServiceComponent(){
 
+    const scrWidth = Dimensions.get('window').width;
 
     return(
         <View style={styles.container}>
             <CreateOrderComponent/>
             {/* <CartComponent/>
             <CompletedOrders/> */}
+            <View style={{height:40, width:scrWidth, flexDirection:"row", alignSelf:"flex-end", alignItems:"center", justifyContent:"center"}}>
+                <View style={{flexDirection:"row", width:"100%", alignItems:"center", alignContent:"center", justifyContent:"space-evenly", alignSelf:"center"}}>
+                <Button onPress={() =>{}} title="Create an Order"></Button>
+                <Button onPress={() =>{}} title="Cart"></Button>
+                <Button onPress={() =>{}} title="My Orders"></Button>
+                </View>
+            </View>
         </View>
     )
 }
@@ -23,7 +32,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         alignContent: 'center',
         justifyContent: 'center',
-        padding:20,
+        paddingTop:20,
         height:"100%"
       },
 });

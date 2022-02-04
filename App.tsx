55a9ccbb40d961bcd88db6d4b525e13d6da369ec
bajohnson9 <brooksjahnson@gmail.com>
2022-02-03 19:44:10 -0500

    added login

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SignInView from './components/sign-in-view';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <SignInView/>
       <StatusBar style="auto" />
     </View>
   );
